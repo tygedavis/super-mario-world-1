449e46e11adc26979b01f938fb9c18d368c5206c
Tyge Davis <tyge.davis@gmail.com>
2026-07-16 13:40:34 -0600

    mystery box logic completed

--- Resources/mystery_box.gd
+++ Resources/mystery_box.gd
@@ -1,32 +1,40 @@
 extends Node2D
 
-const GREEN_MUSHROOM = "green_mushroom"
-const RED_MUSHROOM = "red_mushroom"
-const FIRE_FLOWER = "fire_flower"
-const STAR = "star"
-const items = [GREEN_MUSHROOM, RED_MUSHROOM, FIRE_FLOWER, STAR]
-var item
-const ENABLED = true
+#const GREEN_MUSHROOM = "green_mushroom"
+@onready var red_mushroom_scene = preload("res://Resources/Items/Red_Mushroom.tscn")
+@onready var fire_flower_scene = preload("res://Resources/Items/Fire_Flower.tscn")
+@onready var star_scene = preload("res://Resources/Items/Star.tscn")
+@onready var items = [red_mushroom_scene, red_mushroom_scene, star_scene]
+@onready var item_scene
+var ENABLED = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Determine which random object to display
-	item = items.pick_random()
-	print("Random item is: ", item)
+	item_scene = items.pick_random()
+	print("Random item is: ", item_scene)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
-func _on_bottom_detector_body_entered(body: Node2D) -> void:
+func _on_bottom_detector_body_entered(_body: Node2D) -> void:
 	print("The mystery box has been entered")
-	# Move the box up and down
-	position.y-=10
-	await get_tree().create_timer(.09).timeout
-	position.y+=10
-	
-	# Spawn the random item
-	
-	# Disable Mystery Box
+	if ENABLED:
+		# Move the box up and down
+		position.y-=10
+		await get_tree().create_timer(.09).timeout
+		position.y+=10
+		
+		# Spawn the random item
+		var item_instance = item_scene.instantiate()
+		print("Global position is: ", global_position)
+		print("Item Instance is: ", item_instance)
+		item_instance.position = Vector2(0, -20)
+		add_child(item_instance)
+		
+		# Disable Mystery Box
+		ENABLED = false
+		$ColorRect.visible = true
